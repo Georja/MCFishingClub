56eb16ec7c42fc9b08d02a68a860446f42d80166
Georja <107449947+Georja@users.noreply.github.com>
2022-06-14 14:40:47 +1200

    setup repository

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Home Page-Marsden Cove Fishing Club</title>
+    <style>
+    /*  SECTIONS  */
+.section {
+	clear: both;
+	padding: 0px;
+	margin: 0px;
+}
+
+/*  COLUMN SETUP  */
+.col {
+	display: block;
+	float:left;
+	margin: 1% 0 1% 1.6%;
+}
+.col:first-child { margin-left: 0; }
+
+
+/*  GROUPING  */
+.group:before,
+.group:after {
+	content:"";
+	display:table;
+}
+.group:after {
+	clear:both;
+}
+.group {
+    zoom:1; /* For IE 6/7 */
+}
+
+/*  GRID OF THREE  */
+.span_3_of_3 {
+	width: 100%;
+}
+.span_2_of_3 {
+	width: 66.1%;
+}
+.span_1_of_3 {
+	width: 32.2%;
+}
+
+/*  GO FULL WIDTH AT LESS THAN 480 PIXELS */
+
+@media only screen and (max-width: 600px) {
+	.col { margin: 1% 0 1% 0%;}
+	.span_3_of_3, .span_2_of_3, .span_1_of_3 { width: 100%; }
+}
+    </style>
+</head>
+<body>
+
+<h1>Marsden Cove Fishing Club</h1>
+<header class="section group">
+	<div class="col span_1_of_3">
+	<p>HOME   ABOUT US   JOIN US   GALLERY   EVENTS </p>
+	</div>
+	<div class="col span_1_of_3">
+	<img src="images/mcfc-logo.jpg" alt="mcfc-logo">
+
+	</div>
+	<div class="col span_1_of_3">
+	<p>LEADER BOARDS    WEIGH MASTERS  SPONSORS   SAFETY   CONTACT                                                            FB</p>
+	</div>
+</header>
+    <div class="col span_1_of_3">
+	<p>ABOUT US</p>
+	</div>
+<div class="section group">
+	<div class="col span_1_of_3">
+    <img src="images/marlin-weighing-thin.jpg" alt="weighedmarlin-thin">
+    </div>
+	
+	<div class="col span_2_of_3">
+         <p>Marsden Cove Fishing Club is located at the Marsden Cove Marina (next to Land and Sea Café) and overlooks the marina with prime viewing of the boat ramp — which can provide some entertainment on the odd occasion.
+</p>
+<p>Members, both local and from further afield have created a strong sense of community within the club. It’s a great place to bring the family, meet up with your friends, tell those great fishing stories, and compete for the annual angler of year prize.</p>
+
+<p>To become a member, pop into the club on a Thursday, Friday or Saturday night and fill out a membership form, or complete the online registration form.</p>
+
+	</div>
+    <div class="col span_2_of_3">
+	<p>BUSINESS WALL SIGN</p>
+</div>
+    <div class="col span_2_of_3">
+        <p>For a minimal annual fee of $150.00 +gst  you will have your company sign and logo displayed on the club wall.
+
+Sponsorship renewal annual fee is $130.00 +gst (if we already have your company logo wall plaque)
+
+Contact the club secretary for more details.</p>
+        
+        <div class="col span_2_of_3">
+ <p>CLUB COMITTEE</p>
+ <P>Current committee members details.</P>
+</div>
+<footer class="section group">
+	<div class="col span_3_of_3">
+	Footer
+	</div>
+	
+</footer>
+
+</body>
+</html>
